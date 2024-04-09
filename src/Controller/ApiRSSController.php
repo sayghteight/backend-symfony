df12d57aa1c7433c80b97895a2f85eca199cec14
sayghteight <sayghteight@gmail.com>
2024-04-09 04:59:37 +0200

    feat: update api

--- src/Controller/ApiRSSController.php
+++ src/Controller/ApiRSSController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\RSSSources;
 use App\Repository\RSSSourcesRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
@@ -19,6 +21,7 @@ class ApiRSSController extends AbstractController
 
         foreach ($rssSources as $rssSource) {
             $data[] = [
+                'id' => $rssSource->getId(),
                 'name' => $rssSource->getName(),
                 'url' => $rssSource->getUrl(),
                 'enabled' => $rssSource->getEnabled(),
@@ -28,4 +31,57 @@ class ApiRSSController extends AbstractController
         return new JsonResponse($data, Response::HTTP_OK);
     }
 
+    #[Route('/api/find/{id}', name: 'app_api_find', methods: ['GET'])]
+    public function find(RSSSourcesRepository $rssSourcesRepository, int $id): JsonResponse
+    {
+        $rssSource = $rssSourcesRepository->findBy(['id' => $id]);
+
+        $data[] = [
+            'id' => $rssSource[0]->getId(),
+            'name' => $rssSource[0]->getName(),
+            'url' => $rssSource[0]->getUrl(),
+            'enabled' => $rssSource[0]->getEnabled(),
+        ];
+
+        return new JsonResponse($data, Response::HTTP_OK);
+    }
+
+    #[Route('/api/create', name: 'app_api_create_rss', methods: ['POST'])]
+    public function create(Request $request, EntityManagerInterface $entityManager): JsonResponse
+    {
+        $data = json_decode($request->getContent(), true);
+
+        $rssSource = new RSSSources();
+        $rssSource->setName($data['name']);
+        $rssSource->setUrl($data['url']);
+        $rssSource->setEnabled($data['enabled']);
+
+        $entityManager->persist($rssSource);
+        $entityManager->flush();
+
+        return new JsonResponse(['message' => 'RSS source created'], Response::HTTP_CREATED);
+    }
+
+    #[Route('/api/update/{id}', name: 'app_api_update_rss', methods: ['PUT'])]
+    public function update(Request $request, EntityManagerInterface $entityManager, RSSSources $rssSource): JsonResponse
+    {
+        $data = json_decode($request->getContent(), true);
+
+        $rssSource->setName($data['name']);
+        $rssSource->setUrl($data['url']);
+        $rssSource->setEnabled($data['enabled']);
+
+        $entityManager->flush();
+
+        return new JsonResponse(['message' => 'RSS source updated'], Response::HTTP_OK);
+    }
+
+    #[Route('/api/delete/{id}', name: 'app_api_delete_rss', methods: ['DELETE'])]
+    public function delete(EntityManagerInterface $entityManager, RSSSources $rssSource): JsonResponse
+    {
+        $entityManager->remove($rssSource);
+        $entityManager->flush();
+
+        return new JsonResponse(['message' => 'RSS source deleted'], Response::HTTP_OK);
+    }
 }
